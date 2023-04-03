702f1088f213aa0ff9c1b274c24de23f112c4f17
Zachary <41703755+zknepp1@users.noreply.github.com>
2023-04-03 14:19:58 -0500

    Update main.py

--- project1/main.py
+++ project1/main.py
@@ -11,6 +11,57 @@ from redact import in_matches
 import en_core_web_sm
 
 
+# Functions to Sanitize and Redact 
+def scrub(text):
+    nlp = spacy.load("en_core_web_sm")
+    docx = nlp(text)
+    redacted_sentences = []
+    for token in docx:
+        if token.ent_type_ == 'PERSON':
+            redacted_sentences.append('\u2588')
+
+        elif token.ent_type_ == 'DATE':
+            redacted_sentences.append('\u2588')
+
+        elif token.ent_type_ == 'PHONE':
+            redacted_sentences.append('\u2588')
+
+        elif token.ent_type_ == 'GENDER':
+            redacted_sentences.append('\u2588')
+
+        elif in_matches(token.text):
+          redacted_sentences.append('\u2588')
+
+        else:
+            redacted_sentences.append(token.text)
+
+    return " ".join(redacted_sentences)
+
+    
+def write_to_file(text):
+  tokens = [token.orth_ for token in text]
+  f = open("myfile.redacted", "w")
+  f.write(tokens)
+  #print(f.read())
+  f.close()
+
+
+def in_matches(ent):
+  for match_id, start, end in matches:
+    matched_span = doc[start:end]
+    if matched_span.text == ent:
+      return True
+    #print(matched_span.text)
+
+
+
+
+
+
+
+
+
+
 
 nlp = spacy.load("en_core_web_sm")
 
